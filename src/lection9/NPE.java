01dfe932deba0fc9a972efb918d5bac1567c7d5f
SMD_ASY <andrii.sydorov@googlemail.com>
2021-02-03 21:02:26 +0200

    Some solution from lecture 9

---
+++ src/lection9/NPE.java
@@ -0,0 +1,23 @@
+package lection9;
+
+/**
+ * Перехватить исключение (и вывести его на экран), указав его тип, возникающее
+ * при выполнении кода: 
+ * String s = null; 
+ * String m = s.toLowerCase();
+ * 
+ * @author SMD_ASY
+ *
+ */
+public class NPE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = null;
+		try {
+			String m = s.toLowerCase();
+		} catch (NullPointerException npe) {
+			System.out.println(npe);
+		}
+	}
+}
